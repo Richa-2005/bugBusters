4feea6fcc345560b614f6d9e5e85329282a4f8a3
brijalpatel0 <133280455+brijalpatel0@users.noreply.github.com>
2025-08-30 15:51:44 +0530

    homepage css

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -7,7 +7,9 @@ export default function NavBar() {
     <nav className="navbar">
       <div className="navbar-container">
         <ul className="nav-links">
-        <li className="logo"></li> 
+        <li className="logo">
+          <Link to="/" className="nav-link"></Link>
+          </li> 
           <li className="nav-item">
             <Link to="/" className="nav-link"><b>Home</b></Link> 
           </li> 
